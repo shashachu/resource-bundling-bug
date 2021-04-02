3c50d37addc17194cb4498217eff340a661ec773
Sha Sha Chu <shasha@pinterest.com>
2021-04-01 22:52:25 -0700

    Initial commit of repo demonstrating resource bundling bug

---
+++ app/src/main/java/com/pinterest/baseresources/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.pinterest.baseresources
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
